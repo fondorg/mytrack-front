c1fbd85d0ed6b0bdb35965ded1002d290d0e4731
greek <fondorg@gmail.com>
2021-04-25 12:13:29 +0300

    home page readme

--- frontend/src/c8s/MediumTitle.svelte
+++ frontend/src/c8s/MediumTitle.svelte
@@ -2,4 +2,4 @@
     export let title
 </script>
 
-<h3 class="font-bold text-md mb-2">{title}</h3>
+<h3 class="font-bold text-lg mb-2">{title}</h3>
--- frontend/src/routes/Home.svelte
+++ frontend/src/routes/Home.svelte
@@ -3,7 +3,6 @@
     import Layout from '../c8s/Layout.svelte'
     import CenteredFlex from "../c8s/CenteredFlex.svelte";
     import MediumTitle from '../c8s/MediumTitle.svelte'
-    import MicroTitle from '../c8s/MicroTitle.svelte'
     import ExtLink from '../c8s/ExtLink.svelte'
 
     onMount(() => {
@@ -15,14 +14,13 @@
         <MediumTitle title="MyTrack. Issue tracker"/>
         <div class="mt-5">Check out your <a class="underline" href="#/projects">Projects</a></div>
 
-
         <CenteredFlex extraClasses="mt-10">
             <div class="border-t w-11/12 pb-2"></div>
-            <MicroTitle title="General purpose issue tracker"/>
+            <MediumTitle title="General purpose issue tracker"/>
             <div class="text-justify md:w-3/4 px-2 md:px-0">
                 <div>
                     This is an open source training project that provides issue tracker functionality.
-                    The project currently contains of a couple of separate services. Source can be found here:
+                    The project currently contains a couple of separate services. Source can be found here:
                 </div>
                 <ul class="list-disc">
                     <li>
@@ -43,7 +41,7 @@
                     protocol.
                 </div>
                 <div class="mt-2">
-                    <MicroTitle title="Technology stack"/>
+                    <MediumTitle title="Technology stack"/>
                     <div class="mt-1">Backend:</div>
                     <ul class="list-disc">
                         <li>Spring Boot</li>
@@ -83,7 +81,11 @@
                     </ul>
                 </div>
                 <div class="mt-2">
-                    <MicroTitle title="MyTracker features"/>
+                    <MediumTitle title="Deploy"/>
+                    <div>Each service provides a set of Kubernetes manifests for deployment</div>
+                </div>
+                <div class="mt-2">
+                    <MediumTitle title="MyTracker features"/>
                     <div>Some main MyTracker features currently available</div>
                     <ul class="list-disc">
                         <li>Projects management</li>
